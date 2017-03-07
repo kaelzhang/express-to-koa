34a202e41a1d968f1e6ecdff4a62e0a1769749f9
Kael Zhang <i@kael.me>
2017-03-07 17:46:36 +0800

    1.0.2: fixes write and write head

--- src/index.js
+++ src/index.js
@@ -10,12 +10,30 @@ const properties = {
     }
   },
 
-  end: {
+  writeHead: {
+    value (...args) {
+      console.log('write head')
+      this._explicitStatus = true
+      this._response.writeHead(...args)
+    }
+  },
+
+  write: {
     value (...args) {
       // Koa and Koa2 set the statusCode to `404` by default.
       // So we must do something as well as `ctx.body = body`.
       if (!this._explicitStatus) {
         // set to _response.statusCode
+        this.statusCode = 200
+      }
+
+      return this._response.write(...args)
+    }
+  },
+
+  end: {
+    value (...args) {
+      if (!this._explicitStatus) {
         this.statusCode = 200
       }
 
