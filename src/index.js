1491ea54df86876e4508351ed07cf59aeac9d4a9
yongjiancui(崔永键) <yongjiancui@tencent.com>
2019-03-06 10:47:53 +0800

    fix: remove _explicitStatus setter

--- src/index.js
+++ src/index.js
@@ -1,51 +1,73 @@
-const properties = {
+"use strict";
+
+var properties = {
+  statusCode: {
+    get: function get() {
+      return this._response.statusCode;
+    },
+    set: function set(code) {
+      this._response.statusCode = code;
+    }
+  },
+
+  writeHead: {
+    value: function value() {
+      var _response;
+      (_response = this._response).writeHead.apply(_response, arguments);
+    }
+  },
+
   write: {
-    value (...args) {
+    value: function value() {
+      var _response2;
+
       // Koa and Koa2 set the statusCode to `404` by default.
       // So we must do something as well as `ctx.body = body`.
       if (!this._explicitStatus) {
         // set to _response.statusCode
-        this.statusCode = 200
+        this.statusCode = 200;
       }
 
-      return this._response.write(...args)
+      return (_response2 = this._response).write.apply(_response2, arguments);
     }
   },
 
   end: {
-    value (...args) {
+    value: function value() {
+      var _response3;
+
       if (!this._explicitStatus) {
-        this.statusCode = 200
+        this.statusCode = 200;
       }
 
-      return this._response.end(...args)
+      return (_response3 = this._response).end.apply(_response3, arguments);
     }
   }
-}
+};
 
-
-function makeResponse (res) {
+function makeResponse(res) {
   return Object.create({
     __proto__: res,
-    _response: res
-  }, properties)
+    _response: res,
+
+  }, properties);
 }
 
-
-function wrap (ctx, middleware, next) {
-  return new Promise((resolve, reject) => {
-    middleware(ctx.req, makeResponse(ctx.res), err => {
+function wrap(ctx, middleware, next) {
+  return new Promise(function (resolve, reject) {
+    middleware(ctx.req, makeResponse(ctx.res), function (err) {
       if (err) {
-        reject(err)
-        return
+        reject(err);
+        return;
       }
 
-      resolve(next())
-    })
-  })
+      resolve(next());
+    });
+  });
 }
 
-
-module.exports = function e2k (middleware) {
-  return (ctx, next) => wrap(ctx, middleware, next)
-}
+module.exports = function e2k(middleware) {
+  return function (ctx, next) {
+    return wrap(ctx, middleware, next);
+  };
+};
